60235a14e488f04c2b9d00016c9d4c52b2e59aaa
Ezekiel Elin <ezekielelin@me.com>
2012-09-10 20:11:52 -0400

    Added MIT License Notes
    Correct if wrong, please

--- foot.php
+++ foot.php
@@ -1,8 +1,13 @@
 			<div>
 				<hr />
 				<footer>
+<<<<<<< HEAD
 					 <p>&copy; Hanover High School 2012. Created by Owen Versteeg; A complete list of contributors is <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors">here</a></p>
 					 <p>This website is MIT licensed and available on <a href="https://github.com/HanoverHighSchool/hhswebsite">GitHub</a>.</p>
+=======
+					 <p>&copy; Hanover High School 2012.</p>
+					 <p>This website is MIT Licensed and available on <a href="http://github.com/HanoverHighschool/hhswebsite">Github</a>!
+>>>>>>> Added MIT License Notes
 				</footer>
 			</div>
 		</div>
