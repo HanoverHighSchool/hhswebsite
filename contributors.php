640907c9fc4c7c4b2b676f2dc47bc48047d61ab4
Ezekiel Elin <ezekielelin@me.com>
2012-09-10 20:01:02 -0400

    General Changes

---
+++ contributors.php
@@ -0,0 +1,16 @@
+<index>
+	<head>
+		<title>
+			Contributors to Hanover Highschool Website
+		</title>
+	</head>
+	<body>
+		<list style="list-style-type: none">
+			<li>Ezekiel Elin</li>
+			<li>Glenn Smith</li>
+			<li>Owen Versteeg</li>
+			<li><hr \></li>
+			<li>Want to contribute? Please email Ezekiel through your school email. We are not accepting contributions through other sources at this time.</li>
+		</list>
+	</body>
+</index>
