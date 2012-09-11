24503ff7234f37961420dd1abd5bb2a38065f664
calubedy <cjwinster@gmail.com>
2012-09-11 13:46:38 -0300

    updated url
    now the url points to the new repository...

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
       <div class="span4">
          <h2>Contribute to this website!</h2>
          <p>This new website was created by Owen Versteeg (an HHS student) and now is open-source. If you have something interesting to add to it, simply head on over to GitHub and help out! </p>
-         <p><a class="btn" href="https://github.com/OwenVersteeg/hhswebsite">This website on GitHub &raquo;</a></p>
+         <p><a class="btn" href="https://github.com/HanoverHighSchool/hhswebsite">This website on GitHub &raquo;</a></p>
       </div>
    </div>
 <?php require("foot.php"); ?>
