e20e21198a1c32cfbf4b808dbb3a033451e610f1
marcovg <mvangemjr@comcast.net>
2012-09-12 18:22:42 -0300

    Updated Contributors

--- contributors.php
+++ contributors.php
@@ -9,7 +9,8 @@
 			<li>Ezekiel Elin</li>
 			<li>Glenn Smith</li>
 			<li>Owen Versteeg</li>
-			<li><hr \></li>
+			<li>Caleb Winberry</li>
+			<li>Marco van Gemeren<hr \></li>
 			<li>Want to contribute? Please email Ezekiel through your school email. We are not accepting contributions through other sources at this time.</li>
 		</list>
 	</body>
