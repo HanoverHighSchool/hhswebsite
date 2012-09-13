97dbdb0202fd7cc40308994f10dcb4d97db991d6
Ezekiel Elin <ezekielelin@me.com>
2012-09-13 12:44:50 -0400

    Removed owen from list of creators, changed it to a more broad term.

--- foot.php
+++ foot.php
@@ -1,9 +1,10 @@
 			<div>
 				<hr />
 				<footer>
-					 <p>&copy; Hanover High School 2012. Created by Owen Versteeg; a complete list of contributors is <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors">here</a>.</p>
-					 <p>This website is MIT licensed and available on <a href="https://github.com/HanoverHighSchool/hhswebsite">GitHub</a>.</p>
-					 <br>
+					<!-- OWEN DON"T YOU DARE CHANGE IT BACK! -->
+					<p>&copy; Hanover High School 2012. Created by 9th Graders at HHS. A complete list of contributors is <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors">here</a>.</p>
+					<p>This website is MIT licensed and available on <a href="https://github.com/HanoverHighSchool/hhswebsite">GitHub</a>.</p>
+					<br>
 				</footer>
 			</div>
 		</div>
