2cae7a2244ec3018b04d42df5f39c93ba8554563
marcovg <mvangemjr@comcast.net>
2012-09-13 18:57:01 -0300

    Added /hhs1-final/ to new PDF links

--- head.php
+++ head.php
@@ -83,8 +83,8 @@
 									<li class="dropdown-submenu">
 										<a tabindex="-1" href="#">Bus Route</a>
 										<ul class="dropdown-menu">
-											<li><a tabindex="-1" href="/assets/pdf/Revised_HanoverDresdenTransGuide12-13.pdf">Hanover</a></li>
-											<li><a tabindex="-1" href="/assets/pdf/RevisedNorwichDresdenTransGuide12-13.pdf">Norwich</a></li>
+											<li><a tabindex="-1" href="/hhs1-final/assets/pdf/Revised_HanoverDresdenTransGuide12-13.pdf">Hanover</a></li>
+											<li><a tabindex="-1" href="/hhs1-final/assets/pdf/RevisedNorwichDresdenTransGuide12-13.pdf">Norwich</a></li>
 										</ul>
 									</li>
 									<li><a href="http://hanoverhigh.us/Hanover/calendar.html">Calendar</a></li>
