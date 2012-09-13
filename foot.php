1981ad2860152d41f10440697a524ae193f8c535
marcovg <mvangemjr@comcast.net>
2012-09-13 09:24:33 -0300

    Added Date Stamp

--- foot.php
+++ foot.php
@@ -2,7 +2,13 @@
 				<hr />
 				<footer>
 					 <p>&copy; Hanover High School 2012. Created by Owen Versteeg; a complete list of contributors is <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors">here</a>.</p>
-					 <p>This website is MIT licensed and available on <a href="https://github.com/HanoverHighSchool/hhswebsite">GitHub</a>.</p>				
+					 <p>This website is MIT licensed and available on <a href="https://github.com/HanoverHighSchool/hhswebsite">GitHub</a>.</p>
+					 <br>
+					 <script type="text/javascript">
+					 //<![CDATA[
+					writeDate({day:true, lang:"en"});
+					//]]>
+					</script>
 				</footer>
 			</div>
 		</div>
