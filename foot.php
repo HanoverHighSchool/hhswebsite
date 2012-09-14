0397748db4456f954d2247fdde4540dee20cc0ec
Ezekiel Elin <ezekielelin@me.com>
2012-09-14 10:29:16 -0400

    Revert "changed footer"
    This reverts commit 5d7e7b8bbbc1d074cf909eabb10fe2f0ba2af841.

--- foot.php
+++ foot.php
@@ -1,9 +1,9 @@
 			<div>
 				<hr />
 				<footer>
-					<p>&copy; Hanover High School 2012.</p>
-					<p>Project created by Owen Versteeg; other contributors to this open-source project listed <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors?type=a">here</a>.</p>
+					<p>&copy; Hanover High School 2012. Created by 9th Graders at HHS. A complete list of contributors is <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors">here</a>.</p>
 					<p>This website is MIT licensed and available on <a href="https://github.com/HanoverHighSchool/hhswebsite">GitHub</a>.</p>
+					<br>
 				</footer>
 			</div>
 		</div>
