458ebd4c077ec24a4cd93a0f389b2685ce10a017
Glenn Smith <couleeapps@gmail.com>
2012-09-14 22:26:22 -0300

    Update foot.php
    Clarified on who did what. Owen still gets his name first, and now it sounds cleaner (approved by Ezekiel).

--- foot.php
+++ foot.php
@@ -1,7 +1,7 @@
 			<div>
 				<hr />
 				<footer>
-					<p>&copy; Hanover High School 2012. Most code by Owen Versteeg; much other code written by contributors listed <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors?type=a">here</a>.</p>
+					<p>&copy; Hanover High School 2012. Design, style, and layout by Owen Versteeg; other portions written by contributors listed <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors?type=a">here</a>.</p>
 					<p>This website is MIT licensed and available on <a href="https://github.com/HanoverHighSchool/hhswebsite">GitHub</a>.</p>
 					<br>
 				</footer>
