49daa7d5f48f7658fe99dbe704addfdb161451e6
Owen Versteeg <owen.versteeg@live.com>
2012-09-16 20:17:55 -0400

    readded operamini and smallened it
    woot for jsmin

--- assets/js/operamini.js
+++ assets/js/operamini.js
@@ -1,18 +1,1 @@
-jQuery(document).ready(function($) {
-  if (typeof operamini != 'undefined') {  //check to see if operamini a JS var added by Opera Mini browser exists so other browsers won't error
-    if(operamini) { //this should only work for Opera Mini
-      $('#menuButton').click(function(e){ //bind click which Opera Mini likes better
-        e.preventDefault(); //prevent default action
-        $(this).collapse({ //manually add collapse to the targeted button
-          toggle:true
-        });
-      });
-    }
-  }
-});
-
-$(document).ready(function() {
-  if(operamini) {
-    $('#menuButton').click(function() {});
-  }
-});
+jQuery(document).ready(function(a){if(typeof operamini!="undefined"){if(operamini){a("#menuButton").click(function(b){b.preventDefault();a(this).collapse({toggle:true})})}}});$(document).ready(function(){if(operamini){$("#menuButton").click(function(){})}})
