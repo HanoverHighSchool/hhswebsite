581b3c3a36f6b1573495969c8e671e0f83997daa
Owen Versteeg <owen.versteeg@live.com>
2012-09-16 21:33:20 -0400

    made art page not nothing
    now it's a pretty hero unit and some links. A perfect functional
    replication.

--- art.php
+++ art.php
@@ -1,14 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <title>Hanover Highschool - Art</title>
+      <title>Hanover High School - Art</title>
 
 <!-- Everything in the <head> except for <title> : Also contains the navbar-->      
 <?php require("head.php"); ?>
 
 <!-- Start main container -->
 <div id="container" class="container">
-
-<!-- ART FTW! -->
+   <!-- Main hero unit -->
+    <div class="hero-unit" id="hero-unit">
+      <h1>Hanover High School Art</h1>
+      <p></p>
+      <p>Hanover High School has a vibrant art program, with three different teachers and copious art supplies. </p>
+      <p>You can contact any of the art teachers with their email addresses below (just click!) or go to the art classrooms (to the right of the gym in HHS.) </p>
+   </div>
+   <div class="row">
+      <div class="span6">
+         <h2>Who teaches HHS Art classes?</h2>
+         <p><a href="mailto:Stephanie.Gordon@dresden.us">Stephanie Gordon</a></p>
+		 <p><a href="mailto:Elizabeth.Greene@dresden.us">Elizabeth Greene</a></p>
+		 <p><a href="mailto:Peter.Lange@dresden.us">Peter Lange (Dept. Head)</a></p>
+      </div>
+      <div class="span6">
+         <h2>View HHS Art Galleries!</h2>
+         <p><a href="http://www.hanoverhigh.us/HHS/Galleries/Pages/Pierced.html">Pierced</a></p>
+		 <p><a href="http://www.hanoverhigh.us/HHS/Galleries/Pages/Castings.html">Castings</a></p>
+		 <p><a href="http://www.hanoverhigh.us/HHS/Galleries/Pages/Wire.html">Wire</a></p>
+		 <p><a href="http://www.hanoverhigh.us/HHS/Galleries/Pages/Painting.html">Painting</a></p>
+		 <p><a href="http://www.hanoverhigh.us/HHS/Galleries/Pages/Drawing.html">Drawing</a></p>
+		 <p><a href="http://www.hanoverhigh.us/HHS/Galleries/Pages/Stone.html">Stone</a></p>
+		 <p><a href="http://www.hanoverhigh.us/HHS/Galleries/Pages/mugs.html">Mugs</a></p>
+		 <p><a href="http://www.hanoverhigh.us/HHS/Galleries/Pages/Still_life.html">Still Life</a></p>
+      </div>
+   </div>
 
 <?php require("foot.php"); ?>
