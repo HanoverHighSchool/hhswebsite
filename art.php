04430da3878f19031b7344c3a0339d0e1814a69f
marcovg <mvangemjr@comcast.net>
2012-09-18 23:21:35 -0300

    Added Art Staff w/ Email Links

--- art.php
+++ art.php
@@ -11,8 +11,14 @@
 
    <!-- Main hero unit -->
    <div class="hero-unit" id="hero-unit">
-      <h1>Page Title Here</h1>
-      Page subtitle text here
+      <h1 tag="1">Art</h1>
+      <p tag="2">
+      <a href="mailto:Stephanie.Gordon@dresden.us">Stephanie Gordon</a> 
+      <br>
+      <a href="mailto:Elizabeth.Greene@dresden.us">Elizabeth Greene</a>
+      <br>
+      <a href="mailto:Peter.Lange@dresden.us">Peter Lange</a> (Department Head)
+      </p>
    </div>
    <div class="row">
       Page information here
