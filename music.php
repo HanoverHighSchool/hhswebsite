632795cc58ff51615c1c40e89ceeca2ddcf9947c
marcovg <mvangemjr@comcast.net>
2012-09-18 23:29:01 -0300

    Added Music Staff w/ Email Links

--- music.php
+++ music.php
@@ -11,8 +11,16 @@
 
    <!-- Main hero unit -->
    <div class="hero-unit" id="hero-unit">
-      <h1 tag="1"></h1>
-      <p tag="2"></p>
+      <h1 tag="1">Music</h1>
+      <p tag="2">
+      <a href="mailto:Patty.Armstrong@dresden.us">Patty Armstrong</a> (Ed. Assistant)
+      <br>
+      <a href="mailto:alexander.spivakovsky@dresden.us">Alexander Spivakovsky</a>
+      <br>
+      <a href="mailto:Norman.Wolfe@dresden.us">Norman Wolfe</a>
+      <br>
+      <a href="mailto:Jane.Woods@dresden.us">Jane Woods</a> 
+      </p>
    </div>
    <div class="row" tag="3">
 
