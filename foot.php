469c3bafc4df3058ee80a3ac135eb4e6b097ecfe
Owen Versteeg <owen.versteeg@live.com>
2012-09-20 06:37:41 -0400

    Revert "And, some changes to foot.php"
    This reverts commit 33ebffa122eabf90cccc62571a5e6f630cef039b.

--- foot.php
+++ foot.php
@@ -1,9 +1,7 @@
 			<div editor-enabled="false">
 				<hr />
 				<footer editor-enabled="false">
-					<p editor-enabled="false">&copy; Hanover High School 2012.<br>
-					Original site design by Owen Versteeg<br>
-					A complete list of contributors is <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors" editor-enabled="false">here</a>.</p>
+					<p editor-enabled="false">copy; Hanover High School 2012. Design, style, and layout by Owen Versteeg; other portions written by contributors listed <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors">here</a>.</p>
 					<p editor-enabled="false">This website is MIT licensed and available on <a href="https://github.com/HanoverHighSchool/hhswebsite" editor-enabled="false">GitHub</a>.</p>
 					<br>
 				</footer>
