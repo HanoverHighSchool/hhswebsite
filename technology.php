8f690efaa9c4d01192baf4aa36d579d283c5bf1d
marcovg <mvangemjr@comcast.net>
2012-09-20 09:13:24 -0300

    Corrected Spelling Mistakes
    The word high school must be separated. 

--- technology.php
+++ technology.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <title>Hanover Highschool - Technology</title>
+      <title>Hanover High School - Technology</title>
 
 <!-- Everything in the <head> except for <title> : Also contains the navbar-->
 <?php require("head.php"); ?>
