fb59fb396f2df3c2ba9e9b3a1f53694c982aff85
marcovg <mvangemjr@comcast.net>
2012-09-20 09:14:58 -0300

    Corrected Spelling Mistakes
    The word high school must be separated. 

--- account-edit.php
+++ account-edit.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <title>Hanover Highschool - Account Settings</title>
+      <title>Hanover High School - Account Settings</title>
 
 <!-- Everything in the <head> except for <title> : Also contains the navbar-->
 <?php require("head.php"); ?>
