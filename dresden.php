86c0a8f8fdabfa3caad14bd2191c5d9c647f4d33
marcovg <mvangemjr@comcast.net>
2012-09-20 09:24:30 -0300

    Corrected Spelling Mistakes
    The word high school must be separated. 

--- dresden.php
+++ dresden.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <title>Hanover Highschool - Dresden Plan</title>
+      <title>Hanover High School - Dresden Plan</title>
 
 <!-- Everything in the <head> except for <title> : Also contains the navbar-->
 <?php require("head.php"); ?>
