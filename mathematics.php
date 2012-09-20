66325083f020b7df727e06168c08317eeb6e24d7
marcovg <mvangemjr@comcast.net>
2012-09-20 09:10:31 -0300

    Corrected Spelling Mistakes
    The word high school must be separated. 

--- mathematics.php
+++ mathematics.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <title>Hanover Highschool - Mathematics</title>
+      <title>Hanover High School - Mathematics</title>
 
 <!-- Everything in the <head> except for <title> : Also contains the navbar-->
 <?php require("head.php"); ?>
