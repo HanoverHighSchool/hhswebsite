6dd468e6ba20ee0a25348f7a4aeb3fe9fba75faa
CouleeApps <couleeapps@gmail.com>
2012-10-11 19:15:14 -0400

    Went near the footer with a 10-meter pole
    As recommended by a *teacher*

--- foot.php
+++ foot.php
@@ -1,7 +1,10 @@
 			<div editor-enabled="false">
 				<hr />
 				<footer editor-enabled="false">
-					<p editor-enabled="false">copy; Hanover High School 2012. Design, style, and layout by Owen Versteeg; PHP by Glenn Smith and Ezekiel Elin, other portions written by contributors listed <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors">here</a>.</p>
+					<p editor-enabled="false">Copyright <?php echo(date("Y")); ?> Hanover High School. Design, style, and layout by Owen Versteeg;<br>
+					PHP by Glenn Smith and Ezekiel Elin,<br>
+					Additional edits by Rujing Zha, Marco Van Gemeran, and Caleb Winberry,<br>
+					Other portions written by contributors listed <a href="https://github.com/HanoverHighSchool/hhswebsite/graphs/contributors">here</a>.</p>
 					<p editor-enabled="false">This website is MIT licensed and available on <a href="https://github.com/HanoverHighSchool/hhswebsite" editor-enabled="false">GitHub</a>.</p>
 					<br>
 				</footer>
